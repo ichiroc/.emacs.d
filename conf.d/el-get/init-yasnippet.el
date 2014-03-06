25a6b6a00b3e92f6324d1afb61b5349ad05f2848
NAKATANI Ichiro <i_nakatani@skk-net.com>
2014-03-06 13:48:00 +0900

    Prioritize yas-dropdown-prompt to yas-prmpt-functions

--- conf.d/el-get/init-yasnippet.el
+++ conf.d/el-get/init-yasnippet.el
@@ -4,3 +4,6 @@
 (setq yas-snippet-dirs (remove "~/.emacs.d/snippets" yas-snippet-dirs))
 (add-to-list 'yas-snippet-dirs "~/.emacs.d/conf.d/snippets")
 
+;; prioritize yas-dropdown-prompt to yas-prmpt-functions
+(setq yas-prompt-functions
+      '(yas-dropdown-prompt yas-completing-prompt yas-x-prompt yas-ido-prompt yas-no-prompt))
