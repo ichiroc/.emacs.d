3a9fd548bd4e174484673a15fba64ac2719c8aaa
NAKATANI Ichiro <i_nakatani@skk-net.com>
2014-03-31 12:46:23 +0900

    kick taskviewer when clock-in.

--- conf.d/init-win.el
+++ conf.d/init-win.el
@@ -22,4 +22,25 @@
 (define-key isearch-mode-map [compend] 'w32-isearch-update)
 (define-key isearch-mode-map [kanji] 'isearch-toggle-input-method)
 
+;; Execute TaskViewer for Windows when clocked in.
+(defvar my-org-clock-in-shell-buffer-name "*ORG-CLOCK-IN-BUFFER*")
+(add-hook 'org-clock-in-hook '(lambda () (interactive)
+                                (async-shell-command (concat "taskviewer"
+                                                             " \""
+                                                             (format-time-string "[%H:%M] " org-clock-start-time)
+                                                             (encode-coding-string org-clock-heading 'cp932)
+                                                             "\" "
+                                                             (number-to-string (- (display-pixel-height) 160))
+                                                             " "
+                                                             (number-to-string (- (display-pixel-width)  420)))
+                                                     (get-buffer-create my-org-clock-in-shell-buffer-name))
+                                (delete-other-windows)))
+(add-hook 'org-clock-out-hook '(lambda () (interactive)
+                                 (shell-command "taskkill /im:taskviewer.exe")
+                                 (let ((clock-process-buffer (get-buffer my-org-clock-in-shell-buffer-name)))
+                                   (while (process-live-p
+                                           (get-buffer-process clock-process-buffer))
+                                     (sleep-for 1))
+                                   (kill-buffer clock-process-buffer))))
+
 (provide 'init-win)
