0db902262a7dee95a9fe9e40a2cd814d2820940a
NAKATANI Ichiro <i_nakatani@skk-net.com>
2014-04-23 03:30:49 +0100

    remove settings for evil

--- conf.d/el-get/init-multiple-cursors.el
+++ conf.d/el-get/init-multiple-cursors.el
@@ -1,6 +1,6 @@
 ;;; for evil mode.
 ;;; Fix misaligned cursors issue.
 ;;; https://github.com/magnars/multiple-cursors.el/issues/17
-(setq evil-visual-char 'exclusive)
-(setq evil-move-cursor-back nil)
+;;; (setq evil-visual-char 'exclusive)
+;;; (setq evil-move-cursor-back nil)
 (setq mc/list-file (concat my-emacs-var-dir ".mc-lists.el"))
